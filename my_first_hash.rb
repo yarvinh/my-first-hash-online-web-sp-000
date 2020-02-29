b4a79a1725ca542b8db9d053db62ae56db2c3ada
binding-archive-7591 <yarhumher@hotmail.com>
2020-02-29 03:37:29 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -15,9 +15,12 @@ end
 
 def retrieval
   shipping_manifest = {
-                  "whale bone corsets" => 5,
-                  "porcelain vases" => 2,
-                  "oil paintings" => 3
+                  # "whale bone corsets" => 5,
+                  # "porcelain vases" => 2,
+                  # "oil paintings" => 3
+                  "whale bone corsets" => 1,
+                  "porcelain vases" => 1,
+                  "oil paintings" => 1
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
